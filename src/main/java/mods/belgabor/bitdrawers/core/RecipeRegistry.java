dc6059b3025ada29415f7eb6207d7ff526c5a5f1
Belgabor <belgabor@gmx.de>
2016-08-11 19:38:29 +0200

    Recipes Missing Translation

--- src/main/java/mods/belgabor/bitdrawers/core/RecipeRegistry.java
+++ src/main/java/mods/belgabor/bitdrawers/core/RecipeRegistry.java
@@ -1,10 +1,14 @@
 package mods.belgabor.bitdrawers.core;
 
+import com.jaquadro.minecraft.storagedrawers.StorageDrawers;
 import mod.chiselsandbits.core.ChiselsAndBits;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 /**
  * Created by Belgabor on 19.07.2016.
@@ -13,5 +17,9 @@ public class RecipeRegistry {
     public void init() {
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockRegistry.bitDrawer, 1), "xxx", "zwz", "xyx",
                 'x', new ItemStack(Blocks.STONE), 'y', new ItemStack(ChiselsAndBits.getItems().itemChiselIron), 'z', new ItemStack(Blocks.PISTON), 'w', "drawerBasic"));
+        GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockRegistry.bitDrawer, 1), new ItemStack(StorageDrawers.blocks.compDrawers, 1), new ItemStack(ChiselsAndBits.getItems().itemBlockBit, 1, OreDictionary.WILDCARD_VALUE)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockRegistry.bitController, 1), "xxx", "zwz", "xyx",
+                'x', new ItemStack(Blocks.STONE), 'y', new ItemStack(ChiselsAndBits.getItems().itemChiselDiamond), 'z', new ItemStack(Items.COMPARATOR), 'w', "drawerBasic"));
+        GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockRegistry.bitController, 1), new ItemStack(StorageDrawers.blocks.controller, 1), new ItemStack(ChiselsAndBits.getItems().itemBlockBit, 1, OreDictionary.WILDCARD_VALUE)));
     }
 }
