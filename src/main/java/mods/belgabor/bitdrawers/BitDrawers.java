ac748863c6772cdbdae79bc838c8ce3ce44f498e
Belgabor <belgabor@gmx.de>
2017-03-29 15:09:03 +0200

    Release prep

--- src/main/java/mods/belgabor/bitdrawers/BitDrawers.java
+++ src/main/java/mods/belgabor/bitdrawers/BitDrawers.java
@@ -34,9 +34,9 @@ public class BitDrawers implements IChiselsAndBitsAddon
 {
     public static final String MODNAME = "Drawers & Bits";
     public static final String MODID = "bitdrawers";
-    public static final String VERSION = "0.4";
-    public static final String SD_VERSION = "4.1.0";
-    public static final int[] SD_VERSIONS = {4, 1, 0};
+    public static final String VERSION = "0.41";
+    public static final String SD_VERSION = "4.2.0";
+    public static final int[] SD_VERSIONS = {4, 2, 0};
     
     @SidedProxy(
             clientSide = "mods.belgabor.bitdrawers.client.ClientProxy",
